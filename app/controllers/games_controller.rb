9485f642e9fe533d596be9192084bbd17d0cf598
Jonathan-Miksis <jmiksis13@gmail.com>
2021-10-13 21:40:32 -0400

    add new route with url segment parameters, sum of inputs

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -7,15 +7,22 @@ class GamesController < ApplicationController
   #   else
   #     render json: {message: "Your name doesn't start with A!"}
 
-  def number_guess
-    user_number = params["number"].to_i
-    if user_number > 36
-      message = "Your guess is too high!"
-    elsif user_number < 36
-      message = "Your guess is too low!"
-    elsif user_number == 36
-      message = "You got the correct number!"
-    end
-    render json: {message: message}
+  # def number_guess
+  #   user_number = params["number"].to_i
+  #   if user_number > 36
+  #     message = "Your guess is too high!"
+  #   elsif user_number < 36
+  #     message = "Your guess is too low!"
+  #   elsif user_number == 36
+  #     message = "You got the correct number!"
+  #   end
+  #   render json: {message: message}
+  # end
+
+  def sum_numbers
+    number1 = params["number"].to_i
+    number2 = params["number2"].to_i
+    render json: {sum: number1 + number2}
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   # get "/game" => "games#name"
-  get "/guess_a_number" => "games#number_guess"
+  # get "/guess_a_number" => "games#number_guess"
+
+  get "/numbers/:number/:number2" => "games#sum_numbers"
 end
