5e5d88b0520df8a4f70615bcbe254e40dabda2f7
Jonathan-Miksis <jmiksis13@gmail.com>
2021-10-13 20:32:07 -0400

    add user's name start with 'A', print message

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,8 +1,12 @@
 class GamesController < ApplicationController
 
   def name
-    caps_name = params["name"]
-    render json: {message: "Hello #{caps_name.upcase}"}
+    users_name = params["name"]
+    if users_name[0] == "a"
+    render json: {message: "Hey, your name starts with the first letter of the alphabet!"}
+    else
+      render json: {message: "Your name doesn't start with A!"}
+    end
   end
 
 end
