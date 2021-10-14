2a02f1956adda403c43c711e4039064e82bbb555
Jonathan-Miksis <jmiksis13@gmail.com>
2021-10-13 20:35:34 -0400

    create new route for guessing number game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,12 +1,13 @@
 class GamesController < ApplicationController
 
-  def name
-    users_name = params["name"]
-    if users_name[0] == "a"
-    render json: {message: "Hey, your name starts with the first letter of the alphabet!"}
-    else
-      render json: {message: "Your name doesn't start with A!"}
-    end
-  end
+  # def name
+  #   users_name = params["name"]
+  #   if users_name[0] == "a"
+  #   render json: {message: "Hey, your name starts with the first letter of the alphabet!"}
+  #   else
+  #     render json: {message: "Your name doesn't start with A!"}
 
+  def number_guess
+    render json: {message: "hello"}
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  get "/game" => "games#name"
+  # get "/game" => "games#name"
+  get "/guess_a_number" => "games#number_guess"
 end
